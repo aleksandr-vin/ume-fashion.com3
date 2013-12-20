bd234222cb80461ff856103cb7e3619d96aa85f2
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-12-20 12:17:41 +0400

    Remove RoR files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-UmeFashionCom3::Application.config.secret_token = 'b04b2ae9cf59023cdb5908a3b7a5b97c6d472560a521de9c46acd3c9d74fe5868fc5627246a4432e5a9ddeaf61370cce16d5212423cdb98c7af180a2d1e82e02'
